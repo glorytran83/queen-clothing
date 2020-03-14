4c654834eb12c341f17e541119b52f245e1c669e
Mai Dinh <mandy@Mais-MacBook-Air.local>
2020-03-15 10:23:50 +1100

    update menu item withRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Switch, Route} from 'react-router-dom';
+
+
+
+const HatsPage = () => {
+  return (
+    <div>
+      HatsPage
+    </div>
+  );
+};
+const JacketPage = () => {
+  return (
+    <div>
+      Jackets
+    </div>
+  );
+};
+const SneakersPage = () => {
+  return (
+    <div>
+      SneakersPage
+    </div>
+  );
+};
+const WomensPage = () => {
+  return (
+    <div>
+      WomenPage
+    </div>
+  );
+};const MensPage = () => {
+  return (
+    <div>
+      MensPage
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-    <div className='App'>
-    
-      <HomePage></HomePage>
-      </div>
+    <Switch >
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/Hats' component={HatsPage}></Route>
+    </Switch>
     )}
 }
 
