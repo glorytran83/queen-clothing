c8ed946a56b6276b77ccad145d55e1299e483a66
Mai Dinh <mandy@Mais-MacBook-Air.local>
2020-03-15 12:25:50 +1100

    add images collect page and collection item component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+
 import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
-
-const HatsPage = () => {
-  return (
-    <div>
-      HatsPage
-    </div>
-  );
-};
-const JacketPage = () => {
-  return (
-    <div>
-      Jackets
-    </div>
-  );
-};
-const SneakersPage = () => {
-  return (
-    <div>
-      SneakersPage
-    </div>
-  );
-};
-const WomensPage = () => {
-  return (
-    <div>
-      WomenPage
-    </div>
-  );
-};const MensPage = () => {
-  return (
-    <div>
-      MensPage
-    </div>
-  );
-};
 
 class App extends Component {
   render() {
     return (
     <Switch >
       <Route exact path='/' component={HomePage}></Route>
-      <Route path='/Hats' component={HatsPage}></Route>
+      <Route path='/Shop' component={ShopPage}></Route>
     </Switch>
     )}
 }
